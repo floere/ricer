7a6309c3c2bfce1b78671f50c6b424c7d42c9339
Charlie Somerville <charlie@charliesomerville.com>
2012-07-07 16:02:32 +1000

    don't pass our own cflags through to libuv

--- ext/ricer/extconf.rb
+++ ext/ricer/extconf.rb
@@ -9,7 +9,7 @@ end
 libuv_dir = File.expand_path("../../libuv", __FILE__)
 ricer_dir = File.expand_path("../", __FILE__)
 
-system "cd '#{libuv_dir}'; CFLAGS='#{$CFLAGS}' make; cd '#{ricer_dir}'; cp #{libuv_dir}/uv.a #{ricer_dir}/libuv.a"
+system "cd '#{libuv_dir}'; make; cd '#{ricer_dir}'; cp #{libuv_dir}/uv.a #{ricer_dir}/libuv.a"
 
 dir_config "uv", "#{libuv_dir}/include", ricer_dir
 have_library "uv"
